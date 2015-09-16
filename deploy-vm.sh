385fbbf44be558a5f108f2fe0b81cf662c16deae
Moritz Hesse <moritz.hesse@tu-dresden.de>
2015-09-16 11:11:22 +0200

    Deploy script: Remove .git/ folder after transfer

--- deploy-vm.sh
+++ deploy-vm.sh
@@ -1,7 +1,9 @@
 pscp -r -p * root@SaxID-LDAP-Proxy:/srv/www/htdocs/saxid-ldap-proxy
+ssh root@saxid.zih.tu-dresden.de rm -rf /srv/www/htdocs/saxid-ldap-proxy/.git
 ssh root@saxid.zih.tu-dresden.de chown -R wwwrun:www /srv/www/htdocs/saxid-ldap-proxy
 ssh root@saxid.zih.tu-dresden.de php /srv/www/htdocs/saxid-ldap-proxy/app/console assets:install --symlink -- /srv/www/htdocs/saxid-ldap-proxy/web
 ssh root@saxid.zih.tu-dresden.de php /srv/www/htdocs/saxid-ldap-proxy/app/console cache:clear --env=prod
+
 
 #HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 #setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs
