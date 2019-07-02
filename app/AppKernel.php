fbc490392ae4c10b3c08ae2e3a1fc95ea6ec194d
Jan Frömberg <jan.froemberg@tu-dresden.de>
2019-07-02 18:07:23 +0200

    upgrade to symfony 3.4 from 2.8

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,7 +14,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            //new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Saxid\SimplesamlphpBundle\SaxidSimplesamlphpBundle(),
             new Saxid\SaxidLdapProxyBundle\SaxidLdapProxyBundle(),
         );
@@ -23,7 +23,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            //$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
@@ -32,5 +32,18 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    public function getRootDir()
+    {
+         return __DIR__;
+    }
+    public function getCacheDir()
+    {
+         return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+    public function getLogDir()
+    {
+       return dirname(__DIR__).'/var/logs';
     }
 }
